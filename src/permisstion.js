8fb8b8c9c1af11b92c765b83e3bc2e313ab285fc
tt <xiaotian3303@outlook.com>
2025-12-14 12:47:20 +0800

    feat: 路由鉴权

--- src/permisstion.js
+++ src/permisstion.js
@@ -2,10 +2,38 @@
 import router from '@/router'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
+import useUserStore from '@/stores/modules/user'
+import setting from './setting'
+const userStore = useUserStore()
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
+  document.title = `${setting.title} - ${to.meta.title}`
   nprogress.start()
-  next()
+  const token = userStore.token
+  const username = userStore.username
+  if (token) {
+    if (to.path == '/login') {
+      next({ path: '/' })
+    } else {
+      if (username) {
+        next()
+      } else {
+        try {
+          await userStore.userLogin()
+          next({ ...to })
+        } catch (error) {
+          await userStore.userLogout()
+          next({ path: '/login', query: { redirect: to.path } })
+        }
+      }
+    }
+  } else {
+    if (to.path == '/login') {
+      next()
+    } else {
+      next({ path: '/login', query: { redirect: to.path } })
+    }
+  }
 })
 
 router.afterEach((to, from) => {
