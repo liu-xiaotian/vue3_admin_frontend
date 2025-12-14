da61f4f36733b8fe2e485d0c4a17c85f6056e722
tt <xiaotian3303@outlook.com>
2025-12-14 15:23:25 +0800

    feat: 品牌管理静态页面搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>product/trademark</div>
+  <div>
+    <el-card class="box-card">
+      <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
+      <el-table style="margin: 10px 0" border>
+        <el-table-column label="序号"></el-table-column>
+        <el-table-column label="品牌名称"></el-table-column>
+        <el-table-column label="品牌LOGO"></el-table-column>
+        <el-table-column label="品牌操作"></el-table-column>
+      </el-table>
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="limit"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout="prev, pager, next, jumper, ->, sizes,total"
+        :total="total"
+      />
+    </el-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+let pageNo = ref(1)
+let limit = ref(3)
+let total = ref(60)
+</script>
 
 <style lang="scss" scoped></style>
