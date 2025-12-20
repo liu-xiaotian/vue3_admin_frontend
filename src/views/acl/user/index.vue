9ac961f1a0b5b2ef61e81b2ae0c2e162b7448c52
tt <xiaotian3303@outlook.com>
2025-12-20 16:17:23 +0800

    feat: 用户管理静态搭建

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -1,7 +1,92 @@
 <template>
-  <div>acl/user</div>
+  <el-card style="height: 80px">
+    <el-form :inline="true" class="form">
+      <el-form-item label="用户名:">
+        <el-input placeholder="请你输入搜索用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="default">搜索</el-button>
+        <el-button type="primary" size="default">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card style="margin: 10px 0">
+    <el-button type="primary" size="default">添加用户</el-button>
+    <el-button type="primary" size="default">批量删除</el-button>
+    <el-table style="margin: 10px 0" border>
+      <el-table-column type="selection" align="center"></el-table-column>
+      <el-table-column label="#" align="center" type="index"></el-table-column>
+      <el-table-column label="ID" align="center" prop="id"></el-table-column>
+      <el-table-column
+        label="用户名字"
+        align="center"
+        prop="username"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        label="用户名称"
+        align="center"
+        prop="name"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        label="用户角色"
+        align="center"
+        prop="roleName"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        label="创建时间"
+        align="center"
+        prop="createTime"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        label="更新时间"
+        align="center"
+        prop="updateTime"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column label="操作" width="300px" align="center">
+        <template #default="{ row }">
+          <el-button type="primary" size="small" icon="User" @click="setRole(row)"
+            >分配角色</el-button
+          >
+          <el-button type="primary" size="small" icon="Edit" @click="updateUser(row)"
+            >编辑</el-button
+          >
+          <el-popconfirm
+            :title="`你确定要删除${row.username}?`"
+            width="260px"
+            @confirm="deleteUser(row.id)"
+          >
+            <template #reference>
+              <el-button type="primary" size="small" icon="Delete">删除</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页器 -->
+    <el-pagination
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[5, 7, 9, 11]"
+      :background="true"
+      layout="prev, pager, next, jumper,->,sizes,total"
+      :total="total"
+      @current-change="getHasUser"
+      @size-change="handler"
+    />
+  </el-card>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
